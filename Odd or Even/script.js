eeb3ac7803c4d8b3220f688b40831462452d3eed
Kiril Kostov <kiril.kostov@live.com>
2017-08-12 14:38:44 +0100

    Add files via upload

---
+++ Odd or Even/script.js
@@ -0,0 +1,33 @@
+
+/* Details
+
+Given an array of numbers, determine whether the sum of all of the numbers is odd or even.
+
+Give your answer in string format as 'odd' or 'even'.
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Example:
+
+oddOrEven([0]) returns "even"
+oddOrEven([2, 5, 34, 6]) returns "odd"
+oddOrEven([0, -1, -5]) returns "even"
+
+End Details */
+
+/* --- Solution --- */
+function oddOrEven(array) {
+   var prev = 0;
+
+       if (typeof array !== 'undefined' && array.length > 0) {
+             array.map( (item) => {
+           return prev += item;
+          });
+
+   } else {prev = 0;}
+      if (prev % 2 == 0) {
+        return "even";
+       } else {
+        return "odd";
+       }
+}
