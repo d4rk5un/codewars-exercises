9bd582ccbb30a05eed4c9683258bfe07d619357f
Kiril Kostov <kiril.kostov@live.com>
2017-08-12 14:14:37 +0100

    Add files via upload

---
+++ Exclamation marks/script.js
@@ -0,0 +1,23 @@
+
+/* Details
+
+Remove all exclamation marks from the end of sentence.
+
+Examples
+
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+Note
+
+Please don't post issue about difficulty or duplicate.
+
+End Details */
+
+/* --- Solution --- */
+function remove(s){
+  return s.replace(/!+$/gm,"");
+}
